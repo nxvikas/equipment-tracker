34025283ae7d63e4d935739d1c1c515b49c9df84
nxvikas <nxvikkas.vik@mail.ru>
2026-04-06 18:46:32 +0300

    add database-migration

---
+++ app/Http/Controllers/EquipmentHistoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipment_history;
+use Illuminate\Http\Request;
+
+class EquipmentHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Equipment_history $equipment_history)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Equipment_history $equipment_history)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Equipment_history $equipment_history)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Equipment_history $equipment_history)
+    {
+        //
+    }
+}
